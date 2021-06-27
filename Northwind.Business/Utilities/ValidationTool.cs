bdebbbe002aa735ad8268b4ca3cc6d30fb58295c
Doğukan Uykun <76883066+dogukanuykun@users.noreply.github.com>
2021-06-28 01:10:34 +0300

    Validation error fixed

--- Northwind.Business/Utilities/ValidationTool.cs
+++ Northwind.Business/Utilities/ValidationTool.cs
@@ -12,7 +12,7 @@ namespace Northwind.Business.Utilities
         public static void Validate(IValidator validator,object entity)
         {
             
-            var result = validator.Validate(entity);
+            var result = validator.Validate((IValidationContext)entity);
             if (result.Errors.Count > 0)
             {
                 throw new ValidationException(result.Errors.ToString());
